0596ea527200e46fdc11c3dcd9407b2e7a8c707e
yagmursoft <i.eymenyagmur@icloud.com>
2025-06-28 06:49:51 +0300

    add ψ², position

--- statistics.ts
+++ statistics.ts
@@ -7,3 +7,10 @@ function Hadamard(qubit: Qubit): Qubit {
   const newb = (a - b) / Math.sqrt(2);
   return [newa, newb];
 }
+
+// Measurement ψ² is used
+function measure(qubit: Qubit): 0 | 1 {
+  const [a, b] = qubit;
+  const p0 = a ** 2;
+  return Math.random() < p0 ? 0 : 1;
+}
