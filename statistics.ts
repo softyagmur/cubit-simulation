fdb4edc2ffd9244744e51ba649b9e140923adb41
yagmursoft <i.eymenyagmur@icloud.com>
2025-06-28 06:46:58 +0300

    add Hadamard gate

--- statistics.ts
+++ statistics.ts
@@ -1,1 +1,9 @@
 type Qubit = [number, number];
+
+// Superposition forming Hadamard gate
+function Hadamard(qubit: Qubit): Qubit {
+  const [a, b] = qubit;
+  const newa = (a + b) / Math.sqrt(2);
+  const newb = (a - b) / Math.sqrt(2);
+  return [newa, newb];
+}
